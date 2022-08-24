d190694cc0de22f0ff811b7a624dfdca5bf612e8
Ryan Theisen <rythei@Ryans-MacBook-Pro-2.local>
2022-08-24 10:57:08 -0500

    add basic linear models section

--- _build/jupyter_execute/content/basic_linear_regression/simple_linear_regression.py
+++ _build/jupyter_execute/content/basic_linear_regression/simple_linear_regression.py
@@ -2,3 +2,172 @@
 # coding: utf-8
 
 # # Simple linear regression
+# 
+# Let's return to the infant mortality dataset that we used in the previous section. We again load the dataset using `pandas`:
+
+# In[1]:
+
+
+import pandas as pd
+
+mortality_data = pd.read_csv('datasets/infant_mortality.csv')
+mortality_data = mortality_data.dropna()
+mortality_data.head()
+
+
+# Now we can transform the features `income` and `infant_mortality` to the log scale, and plot a joint scatter plot of them.
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# calling .to_numpy() on a column of a DataFrame gives you a numpy array back
+log_income = np.log(mortality_data["income"].to_numpy())
+log_infant_mortality = np.log(mortality_data["infant_mortality"].to_numpy())
+
+sns.jointplot(x=log_infant_mortality, y=log_income)
+plt.xlabel("log(Infant mortality rate)", fontsize=12)
+plt.ylabel("log(Per-capita income)", fontsize=12)
+plt.show()
+
+
+# ## Computing correlation
+# Suppose we wanted to quantify the negative relationship between log-mortality and log-income. A simple way to do this would be with the Pearson correlation. For two vectors of observations $(x_1,\dots, x_n)$ and $(y_1,\dots,y_n)$, their Pearson correlation is computed as
+# 
+# $$
+# r = \frac{\text{Cov}(X, Y)}{\sqrt{\text{Var}(X)\text{Var}(Y)}} = \frac{\sum_{i=1}^n (x_i - \bar{x})(y_i-\bar{y})}{\sqrt{\sum_{i=1}^n (x_i-\bar{x})^2 \sum_{i=1}^n (y_i - \bar{y})^2}}.
+# $$
+# 
+# Let's write a simple python function that computes the pearson correlation.
+
+# In[3]:
+
+
+def pearson_corr(x,y):
+    # first compute the means of the two vectors
+    x_bar = np.mean(x)
+    y_bar = np.mean(y)
+
+    # compute numerator (1/n)*\sum_i (x_i - x_bar)*(y_i - y_bar)
+    cov_xy = np.sum((x-x_bar)*(y-y_bar))/len(x)
+
+    # compute the variance of x and y
+    var_x = np.sum((x-x_bar)**2)/len(x)
+    var_y = np.sum((y-y_bar)**2)/len(y)
+
+    # compute the correlation
+    r = cov_xy/np.sqrt(var_x*var_y)
+
+    return r
+
+
+# Now we can use this function to compute the correlation between log-income and log-mortality:
+
+# In[4]:
+
+
+r = pearson_corr(log_income, log_infant_mortality)
+r
+
+
+# Note that the Pearson correlation is bound between -1 and 1, with -1 indicating a perfect negative relationship between the features, and +1 indicating a perfect positive relationship between the features. Thus the observed value of $\approx -.71$ suggesting, as we can see visually, a strong negative relationship between log-infant mortality and log-per-capita income.
+# 
+# Of course, we don't really need to implement our own python function every time we want to compute the Pearson correlation. It's conveniently implemented in the `scipy.stats` library in python (which also includes a number of other methods that will be useful for statistics).
+
+# In[5]:
+
+
+from scipy.stats import pearsonr
+
+r_2, _ = pearsonr(log_income, log_infant_mortality)
+r_2
+
+
+# Indeed, this method gives us the same result as we got with our own function.
+# 
+# ## Finding the line of best fit by minimizing the sum of squares
+# Instead of just computing the correlation between the two features, we could also try to model the relationship by fitting a line to data. In particular, we want to find numbers $\alpha$ and $\beta$ such that
+# 
+# $$
+# \log(\text{per-capita income}) \approx \alpha + \beta \cdot \log(\text{infant mortality})
+# $$
+# 
+# To do this, we just need to find the values of $\alpha$ and $\beta$. Let's denote our obsevations of log-per-capita income by $(x_1,\dots,x_n)$ and those of log-infant mortality as $(y_1,\dots,y_n)$. Then one way to quantiatively choose $\alpha,\beta$ is by minimizing the sum of squared errors:
+# 
+# $$
+# \text{SSE}(\alpha, \beta) = \frac{1}{n}\sum_{i=1}^n (y_i - (\alpha +\beta x_i))^2
+# $$
+# 
+# With a little bit of calculus, we can check that
+# 
+# $$
+# \begin{align*}
+# \frac{\partial}{\partial \alpha}\text{SSE} &= -2\sum_{i=1}^n (y_i - \alpha - \beta x_i) && \text{and}\\
+# \frac{\partial}{\partial \beta}\text{SSE} &= -2\sum_{i=1}^n x_i(y_i - \alpha - \beta x_i)
+# \end{align*}
+# $$
+# 
+# If we set these two derivatives equal to zero (to find the minimizers), we obtain the estimates $\hat{\alpha}$ and $\hat{\beta}$:
+# 
+# $$
+# \begin{align*}
+# \hat{\alpha} &= \bar{y} - \beta \bar{x}\\
+# \hat{\beta} &= \frac{\sum_{i=1}^n (x_i - \bar{x})(y_i - \bar{y})}{\sum_{i=1}^n (x_i-\bar{x})^2}
+# \end{align*}
+# $$
+# 
+# Note that the expression for $\hat{\beta}$ looks very reminiscent of our equation for the Pearson correlation. Let's write a python function that takes in two arrays $(x_1,\dots,x_n)$ and $(y_1,\dots,y_n)$ computes the estimates $\hat{\alpha}$ and $\hat{\beta}$.
+
+# In[6]:
+
+
+def fit_line(x, y):
+    # first compute the means of the two vectors
+    x_bar = np.mean(x)
+    y_bar = np.mean(y)
+
+    # compute numerator (1/n)*\sum_i (x_i - x_bar)*(y_i - y_bar)
+    cov_xy = np.sum((x-x_bar)*(y-y_bar))/len(x)
+
+    # compute the variance of x and y
+    var_x = np.sum((x-x_bar)**2)/len(x)
+
+    # compute beta_hat
+    beta_hat = cov_xy/var_x
+
+    # compute alpha_hat
+    alpha_hat = y_bar - beta_hat*x_bar
+
+    return alpha_hat, beta_hat
+
+
+# Now let's use this to compute the coefficients for the line best fititng our data.
+
+# In[7]:
+
+
+alpha_hat, beta_hat = fit_line(log_infant_mortality, log_income)
+alpha_hat, beta_hat
+
+
+# The intercept term $\hat{\alpha} \approx 10.02$ indicates that the best fit line crosses the y-axis at $y\approx 10.02$. The coefficient for the slope $\hat{\beta} \approx -0.98$ again indicates the negative relationship between log-income and log-infant mortality. Let's overlay this line on top of our joint scatterplot.
+
+# In[8]:
+
+
+xx = np.linspace(2,7,5)
+yy = alpha_hat + beta_hat*xx
+plt.scatter(log_infant_mortality, log_income)
+plt.plot(xx, yy, color='red', linestyle='--', label='line of best fit')
+plt.xlabel("log(Infant mortality rate)", fontsize=12)
+plt.ylabel("log(Per-capita income)", fontsize=12)
+plt.legend()
+plt.show()
+
+
+# ## Interpreting the coefficients of a line of best fit
+# 
+# **todo**
--- _build/jupyter_execute/content/basic_linear_regression/visualizing_data.py
+++ _build/jupyter_execute/content/basic_linear_regression/visualizing_data.py
@@ -3,7 +3,7 @@
 
 # # Exploring and visualizing data
 # 
-# Throughout this course, we will examine a number of datasets and develop techniques to quantify relationships between observed data. However, before we develop quantitative models of data, it is often useful to first develop a _qualitative_ understanding of the relationships present in data, which we can then use to formulate hypotheses that can be tested using quantitive models. 
+# Throughout this course, we will examine a number of datasets and develop techniques to quantify relationships between observed data. However, before we develop quantitative models of data, it is often useful to first develop a _qualitative_ understanding of the relationships present in data, which we can then use to formulate hypotheses that can be tested using quantitive models.
 # 
 # Visualization is often an extremely useful tool for uncovering relationships within data. In this section, we walk through several examples of exploring and visualizing data in python.
 # 
@@ -29,9 +29,17 @@ mortality_data = pd.read_csv('datasets/infant_mortality.csv')
 mortality_data.head()
 
 
-# When you read a dataset in python using `pandas`, it is stored as as `pandas.DataFrame` object, which we can check using the `type` function:
+# There are a few observations where infant mortality is not reported, so let's remove these from the dataset.
 
 # In[3]:
+
+
+mortality_data = mortality_data.dropna()
+
+
+# When you read a dataset in python using `pandas`, it is stored as as `pandas.DataFrame` object, which we can check using the `type` function:
+
+# In[4]:
 
 
 type(mortality_data)
@@ -39,36 +47,38 @@ type(mortality_data)
 
 # Dataframes are very convenient ways to work with data, as we will see throughout this course.
 # 
-# The infant mortality dataset contains the following information about 105 countries: the per-capita income, the infant mortality rate, its region in the world, and whether or not the country is oil-exporting.
+# The infant mortality dataset contains the following information about 101 countries (after removing 4 without available infant mortality rates): the per-capita income, the infant mortality rate, its region in the world, and whether or not the country is oil-exporting.
 # 
 # ## Basic visualization
 # 
 # For now, let's focus on the variables `infant_mortality` and `income`. A histogram is a common way to visualize the distributions of these features, which we can create using the package `matplotlib`.
 
-# In[4]:
+# In[5]:
 
 
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-plt.hist(mortality_data["infant_mortality"], bins=20, density=True)
+plt.hist(mortality_data["infant_mortality"], bins=10, density=True)
 plt.xlabel("Infant mortality rate", fontsize=12)
 plt.ylabel("Density", fontsize=12)
 plt.show()
 
 
-# In[5]:
+# In[6]:
 
 
-plt.hist(mortality_data["income"], bins=20, density=True)
+plt.hist(mortality_data["income"], bins=10, density=True)
 plt.xlabel("Per-capita income", fontsize=12)
 plt.ylabel("Density", fontsize=12)
 plt.show()
 
 
-# This gives us a sense of the types of values the variables `infant_mortality` and `income` can take on, but what about the relationship between these two features? A simple way to inspect this relationship is by creating a scatter plot.
+# A couple things to note from the `matplotlib` side: first, we can specify the number of bins using the `bins` argument -- choosing an appropriate number of bins is often important when making qualitative assessments of a distribution. Second, we use this argument `density=True`, which will automatically normalize the distribution so that the sum of the mass in each bin is equal to 1. If we use `density=False` (which is the default), we will instead get the counts of number of samples in each bin on the y-axis.
+# 
+# These histograms give us a sense of the types of values the variables `infant_mortality` and `income` can take on individually, and we will discuss this more shortly. But what about the relationship between these two features? A simple way to inspect this relationship is by creating a scatter plot.
 
-# In[6]:
+# In[7]:
 
 
 plt.scatter(mortality_data["infant_mortality"], mortality_data["income"])
@@ -77,9 +87,22 @@ plt.ylabel("Per-capita income", fontsize=12)
 plt.show()
 
 
-# From this plot, we observe that, in general, there is a negative relationship between infant mortality rate and per-capita income -- richer countries tend to have lower infant mortality rates. However, there are a few outliers: countries that have high income _and_ high infant mortality. We can quickly find these by looking back at the data.
+# If we want a slightly prettier figure, we can actually combine the joint scatter plots and the histograms into a single plot using the library `seaborn`.
 
-# In[7]:
+# In[8]:
+
+
+import seaborn as sns
+
+sns.jointplot(x=mortality_data["infant_mortality"], y=mortality_data["income"])
+plt.xlabel("Infant mortality rate", fontsize=12)
+plt.ylabel("Per-capita income", fontsize=12)
+plt.show()
+
+
+# From the joint plot,  we observe that in general there is a negative relationship between infant mortality rate and per-capita income -- richer countries tend to have lower infant mortality rates. However, there are a few outliers: countries that have high income _and_ high infant mortality. We can quickly find these by looking back at the data.
+
+# In[9]:
 
 
 mortality_data[mortality_data["infant_mortality"] > 280]
@@ -87,7 +110,7 @@ mortality_data[mortality_data["infant_mortality"] > 280]
 
 # We see that the two outliers are Libya and Saudi Arabia. Interestingly, we note these two countries are both oil exporting countries. So perhaps we may hypothesize that the relationship between infant mortality and per-capita income depends on whether or not the countries are oil exporting. Let's plot these features for oil-exporting countries only to see if this is indeed the case.
 
-# In[8]:
+# In[10]:
 
 
 # select only the oil exporting countries
@@ -103,45 +126,58 @@ plt.show()
 # 
 # ## Transforming data
 # 
-# Note that while the relationship we observed between infant mortality and per-capita income was indeed decreasing, it was not _linear_ -- by which we mean that the data does not appear to lie approximately on a line (we will get to be much more formal about this in subsequent sections).
+# Let's return again to the histograms we plotted earlier of income and infant mortality. Notice from this histograms that both infant mortality and per-capita income are heavily skewed to the right -- meaning that there are many outliers on the high end of either variable. This also distorts the joint plot by making the relationship more exteme on the low income/high mortality and high income/low mortality ends. As we will see later, when we want to build a statistical model for this relationship, it will often make things easier to first remove the skew in our data.
 # 
-# This is primarily because the histograms of infant mortality and per-capita income were heavily skewed -- meaning that there are many outliers on the high end of either variable. One way that we can mitigate this is by _transforming_ the features. Since the issue is with a few large values, we should consider transformations that "squish" large values. For example we might consider either the square root or log transformations, which both have this property.
+# One way that we can mitigate skew is by _transforming_ the features. Since the issue is with a few large values, we should consider transformations that "squish" large values. For example we might consider one of the following transformations which has this property: $T(x)=\log(x), T(x) = \sqrt{x}$  or more generally $T(x) = |x|^{1/p}$ for $p\geq 1$.
 # 
-# Let's see what happens to the histograms when we perform these transformations to the per-capita income variable.
+# Let's see what happens to the histograms when we perform these transformations to the per-capita income and infant mortality variables.
 
-# In[9]:
+# In[11]:
 
 
 import numpy as np
 
-fig, axs = plt.subplots(nrows= 1, ncols=3, figsize=(12,6))
-axs = axs.flatten()
+fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(12,6))
 
-axs[0].hist(mortality_data["income"], bins=10, density=True)
-axs[0].set_xlabel("sqrt(per-capita income)", fontsize=12)
-axs[0].set_ylabel("Density", fontsize=12)
+axs[0,0].hist(np.sqrt(mortality_data["income"]), bins=10, density=True)
+axs[0,0].set_xlabel("sqrt(per-capita income)", fontsize=12)
+axs[0,0].set_ylabel("Density", fontsize=12)
 
-sqrt_income = np.sqrt(mortality_data["income"])
-axs[1].hist(sqrt_income, bins=10, density=True)
-axs[1].set_xlabel("sqrt(per-capita income)", fontsize=12)
-axs[1].set_ylabel("Density", fontsize=12)
+axs[0,1].hist(mortality_data["income"]**(1./3), bins=10, density=True)
+axs[0,1].set_xlabel("(per-capita income)^(1/3)", fontsize=12)
+axs[0,1].set_ylabel("Density", fontsize=12)
 
-log_income = np.log(mortality_data["income"])
-axs[2].hist(log_income, bins=10, density=True)
-axs[2].set_xlabel("log(per-capita income)", fontsize=12)
-axs[2].set_ylabel("Density", fontsize=12)
+axs[0,2].hist(np.log(mortality_data["income"]), bins=10, density=True)
+axs[0,2].set_xlabel("log(per-capita income)", fontsize=12)
+axs[0,2].set_ylabel("Density", fontsize=12)
+
+axs[1,0].hist(np.sqrt(mortality_data["infant_mortality"]), bins=10, density=True)
+axs[1,0].set_xlabel("sqrt(infant mortality)", fontsize=12)
+axs[1,0].set_ylabel("Density", fontsize=12)
+
+axs[1,1].hist(mortality_data["infant_mortality"]**(1./3), bins=10, density=True)
+axs[1,1].set_xlabel("(infant mortality)^(1./3)", fontsize=12)
+axs[1,1].set_ylabel("Density", fontsize=12)
+
+axs[1,2].hist(np.log(mortality_data["infant_mortality"]), bins=10, density=True)
+axs[1,2].set_xlabel("log(infant mortality)", fontsize=12)
+axs[1,2].set_ylabel("Density", fontsize=12)
 
 plt.tight_layout()
 plt.show()
 
 
-# From inspecting these histograms, we see that the square root transformation doesn't quite resolve the skewness, while the log transformation does. Let's verify this by plotting a new scatterplot of log-per-capita income versus log-infant mortality.
+# From these plots it seems as though the log transformation does the best job of removing skew from the income and infant mortality features. Let's look at another joint plot of income and infant mortality, but this time on the log scale.
 
-# In[10]:
+# In[12]:
 
 
-plt.scatter(np.log(mortality_data["infant_mortality"]), np.log(mortality_data["income"]))
-plt.xlabel("log(infant mortality rate)", fontsize=12)
-plt.ylabel("log(per-capita income)", fontsize=12)
+sns.jointplot(x=np.log(mortality_data["infant_mortality"]), y=np.log(mortality_data["income"]))
+plt.xlabel("log(Infant mortality rate)", fontsize=12)
+plt.ylabel("log(Per-capita income)", fontsize=12)
 plt.show()
 
+
+# With the new transformed features, we can still see the two outliers we identified earlier (Libya and Saudi Arabia), but now the general relationship between log-per-capital income and log-infant mortality appears to be roughly linear. In the next section, we will see that we can use this observation to build a model for the relationship between income and infant mortality.
+# 
+# **add some general rules for how to transform features and how to interpret changes in the transformed features**
